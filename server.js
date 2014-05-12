cbdb578154a7db8bee3ad8b49dec276490f513f7
Kurt Medley <kurtmedley@comcast.net>
2014-05-11 21:45:46 -0700

    pjax framework

--- server.js
+++ server.js
@@ -5,20 +5,35 @@
 var express = require('express'),
     exphbs  = require('express3-handlebars'),
     util = require('util'),
+    colors = require('colors'),
+    pjax = require('express-pjax'),
     app = express(),
     hbs = exphbs.create({ /* config */ });
 
-app.use(util);
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use("/static-assets/css/", express.static(__dirname + '/static-assets/css/'));
 
+app.use(util);
+app.use(pjax());
+app.use("/static-assets/css/", express.static(__dirname + '/static-assets/css/'));
+app.use("/static-assets/js/", express.static(__dirname + '/static-assets/js/'));
+app.enable('verbose errors');
 // Handle requests
-app.get('/', function(req, res){
-  res.render('index', {
+app.get('/:var(home|index)?', function(req, res){
+  res.renderPjax('index', {
   	page : "index"
   });
 });
 
+app.get('/book', function(req, res) {
+	res.renderPjax('book', {
+		page: "book"
+	});
+});
+
+app.get('*', function(req,res) {
+	res.status(404).send('<h1 style="text-align: center;">404 Not fizound</h1>');
+});
+
 app.listen(3000);
-util.log("Server listening on 3000");
+util.log("Server listening on 3000".bold.blue);
---
+++ static-assets/js/app.js
@@ -0,0 +1,25 @@
+if (!window.jQuery === 'undefined') {
+	throw new Error('app.js requires jQuery');
+} else {
+	var app = (function($) {
+		function pjx() {
+			$(document).pjax('a[data-pjax]', '#pjax-container')
+			$(document).on('pjax:send', function() {
+  				$('#loading').show()
+			})
+			$(document).on('pjax:complete', function() {
+  				$('#loading').hide();
+			});
+		}
+		return {
+			init : function() {
+				pjx();
+			}
+		};
+	})(jQuery);
+
+	$(document).ready(function() {
+		app.init();
+	});	
+}
+ 
