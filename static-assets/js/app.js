c7708d72084384bc4f431158e4444f2bccf854bf
Kurt Medley <kurtmedley@comcast.net>
2014-09-24 22:01:26 -0700

    js syntax highlighting

--- static-assets/js/app.js
+++ static-assets/js/app.js
@@ -2,18 +2,31 @@ if (!window.jQuery === 'undefined') {
 	throw new Error('app.js requires jQuery');
 } else {
 	var app = (function($) {
+		var isListsjs = false;
 		function pjx() {
 			$(document).pjax('a[data-pjax]', '#pjax-container')
 			$(document).on('pjax:send', function() {
   				$('#loading').show()
 			})
-			$(document).on('pjax:complete', function() {
+			$(document).on('pjax:end', function() {
   				$('#loading').hide();
+  				$('#listsjs').length>0 && !isListsjs 
+  					? highlight() : isListsjs = false
 			});
+		}
+		function highlight() {
+			if ($('#listsjs').length>0) {
+				isListsjs = true; 
+				$('div.js').each(function(i, block) {
+					$(block).text($(block).text().trim()) 
+				 	hljs.highlightBlock(block);
+				});
+			}
 		}
 		return {
 			init : function() {
 				pjx();
+				highlight();
 			}
 		};
 	})(jQuery);
