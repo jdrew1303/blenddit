166485636615d245f3c27de21006d6f27fc72d2e
Kurt Medley <kurtmedley@comcast.net>
2014-09-25 19:10:12 -0700

    adding sidebar navigation

--- static-assets/js/app.js
+++ static-assets/js/app.js
@@ -2,7 +2,6 @@ if (!window.jQuery === 'undefined') {
 	throw new Error('app.js requires jQuery');
 } else {
 	var app = (function($) {
-		var isListsjs = false;
 		function pjx() {
 			$(document).pjax('a[data-pjax]', '#pjax-container')
 			$(document).on('pjax:send', function() {
@@ -10,23 +9,42 @@ if (!window.jQuery === 'undefined') {
 			})
 			$(document).on('pjax:end', function() {
   				$('#loading').hide();
-  				$('#listsjs').length>0 && !isListsjs 
-  					? highlight() : isListsjs = false
+  				if ($('#listsjs').length>0) {
+  					listsjs();
+  				} else {
+  					$('#listsjsIndex').hide()
+  					$('.navbar-brand > .text-warning').text('kurtlocker.org')
+  				}
 			});
 		}
-		function highlight() {
+		function listsjs() {
 			if ($('#listsjs').length>0) {
-				isListsjs = true; 
-				$('div.js').each(function(i, block) {
+				$('#listsjsIndex').show()
+				$('.navbar-brand > .text-warning').text('[ l [ i [ s ] t ] s ]')  
+				$('div.js').each(function(i, block) { // code hightlight
 					$(block).text($(block).text().trim()) 
 				 	hljs.highlightBlock(block);
 				});
+				$('[data-toggle=offcanvas]').unbind('click').click(function() { // sidebar toggle
+			    	$('.row-offcanvas').toggleClass('active');
+			  	});
+			  	$('.sidebarFunc').unbind('click').click(function() {
+			  		$("html, body").animate({ scrollTop: $('#function-'+$(this).attr('num')).offset().top-50 }, 500);
+			  	})
+			  	$('.main-content').unbind('click').click(function() {
+			  		if ($('.row-offcanvas').hasClass('active')) $('.row-offcanvas').removeClass('active');
+			  	})
+			  	$(window).unbind('resize').resize(function() { // fixed sidebar positioning
+			  		$('.affix').width($('#sidebar').width())
+			  		$('.affix').css('max-height',document.documentElement.clientHeight-$('.navbar-header').height()-100)
+			  	});
+			  	$(window).trigger("resize");
 			}
 		}
 		return {
 			init : function() {
 				pjx();
-				highlight();
+				listsjs();
 			}
 		};
 	})(jQuery);
