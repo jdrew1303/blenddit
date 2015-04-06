d259c8f0a00fdf8c3bb09832a1fb179a92794c38
Kurt Medley <kurtmedley@kurt-medleys-mac-pro.local>
2015-04-05 22:15:36 -0700

    visibility change, autorefresh stub out

--- static-assets/js/app.js
+++ static-assets/js/app.js
@@ -390,6 +390,7 @@ var app = (function($) {
 				util.fn.setInCookie('config', config);
 				buildConfigToUI(true);
 			})
+			visibilityChange();
 			$('[data-toggle="tooltip"]').tooltip();
 			redditNames.initialize();
 			startBlending();
@@ -397,6 +398,36 @@ var app = (function($) {
 			subredditSearch('#subreddit-search', true);
 			columnsOrHomeButton();
 		}
+	}
+	function visibilityChange(){
+		var hidden = "hidden";
+		if (hidden in document) {
+			document.addEventListener("visibilitychange", onchange);
+		} else if ((hidden = "mozHidden") in document) {
+			document.addEventListener("mozvisibilitychange", onchange);
+		} else if ((hidden = "webkitHidden") in document) {
+			document.addEventListener("webkitvisibilitychange", onchange);
+		} else if ((hidden = "msHidden") in document) {
+			document.addEventListener("msvisibilitychange", onchange);
+		} else if ("onfocusin" in document) {
+			document.onfocusin = document.onfocusout = onchange;
+		} else {
+			window.onpageshow = window.onpagehide
+		    = window.onfocus = window.onblur = onchange;
+		}
+		function onchange(evt) {
+			var v = "visible", h = "hidden",
+			evtMap = {focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h};
+		    evt = evt || window.event;
+		    evt.type in evtMap
+		    	? evtMap[evt.type] == "hidden" ? autoRefresh(false) : void 0
+		    	: this[hidden] ? autoRefresh(false) : void 0;
+		  }
+		if(document[hidden] !== undefined)
+			onchange({type: document[hidden] ? "blur" : "focus"});
+	}
+	function autoRefresh(bool, except) {
+
 	}
 	function columnsOrHomeButton() {
 		function buttonType() {
