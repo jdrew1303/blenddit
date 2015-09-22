dbc8eb52a7b24cbd389bffe30eab6f143c0e016a
kurtlocker <kurtdmedley@gmail.com>
2015-09-22 16:08:03 -0700

    building a context session object for a user. externalizing it in utility.ware.js

--- app_routes/routes.js
+++ app_routes/routes.js
@@ -9,23 +9,9 @@ module.exports = function(app, globalware, elseware, kutil) {
 		all = gware.methods.concat(kutil.getMethods(mware));
 	
 	app.get('/', gware.nowww, function(req, res){
-		var json = {};
-		json.reddit = {
-			redditUserExists : req.session.reddit ? true : false,
-			redditUser : req.session.reddit ? req.session.reddit.name : '',
-			subredditURI : req.session.subreddit,
-			threadidURI : req.session.threadid,
-			threadidsURI : req.session.threadids
-		};
-		json.twitter = {
-			twitterUserExists : req.session.twitter ? true : false,
-			twitterUser : req.session.twitter ? req.session.twitter.username : '',
-			userAvatar : req.session.twitter ? req.session.twitter.photos[0].value : ''
-		};
-		json.pressType = kutil.getPressType(req.headers['user-agent']);
-		res.renderPjax('blenddit', json);
+		var session = kutil.buildSessionObject(req);
+		res.renderPjax('blenddit', session);
 		req.session.subreddit = req.session.threadid = req.session.threadids = null;
-		// req.session.destroy();
 	});
 	
     app.get('/comments/:threadids*', gware.nowww, function(req, res) {
