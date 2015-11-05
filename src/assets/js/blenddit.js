72146bda08485de3a959210037c3ef459a02c5ab
Kurt Medley <kmedley@corp.costco.com>
2015-11-05 10:45:33 -0800

    nothing significant

--- src/assets/js/blenddit.js
+++ src/assets/js/blenddit.js
@@ -26,13 +26,13 @@ function main_start_slick() {
 		dots: true,
 		infinite: false,
 		speed: 300,
-		slidesToShow: 4,
-		slidesToScroll: 4,
+		slidesToShow: 3,
+		slidesToScroll: 1,
 		responsive: [
 			{
 				breakpoint: 1024,
 				settings: {
-					slidesToShow: 3,
+					slidesToShow: 2,
 					slidesToScroll: 3,
 					infinite: false,
 					dots: true
@@ -42,7 +42,7 @@ function main_start_slick() {
 				breakpoint: 600,
 				settings: {
 					slidesToShow: 2,
-					slidesToScroll: 2
+					slidesToScroll: 1
 				}
 			},
 			{
